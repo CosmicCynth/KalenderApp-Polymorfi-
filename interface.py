59d15d25f16dc1e364928799fdadef85d304f1bb
CosmicCynth <theshinyseal@gmail.com>
2025-12-02 08:59:20 +0100

    Working on button functions

--- interface.py
+++ interface.py
@@ -1,13 +1,19 @@
 class Button:
-    def __init__(self, x, y, width, height, text):
+    def __init__(self, x, y, width, height, text, scene):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.text = text
+        self.scene = scene
+
         self.colors = (255, 255, 255)
         self.textColor = (0,0,0)
 
     def draw_text(self, screen, font, color):
         img = font.render(self.text, True, color)
         screen.blit(img, (self.x, self.y))
+
+    def is_clicked(self):
+        return "labubu"
+
--- main.py
+++ main.py
@@ -1,34 +1,53 @@
 import pygame
-from interface import Button
+
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((400, 800))
 clock = pygame.time.Clock()
 running = True
 
+
+
+# App setup
+global Scene
+Scene = "kalender"
+
+global mousePos
+mousePos = pygame.mouse.get_pos()
+
+#Modules
+from interface import Button
 
 pygame.display.set_caption("Kalender App")
 text_font = pygame.font.SysFont("Arial", 24)
 # Buttons
 Buttons = []
-Buttons.append(Button(100,100,200,64,"Begivenhed"))
+Buttons.append(Button(100,100,200,64,"Begivenhed","kalender")) # X,Y, Width, Height, Text, Scene
 
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+    # Updating mouse position
+    mousePos = pygame.mouse.get_pos()
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
-    for button in Buttons:
-        pygame.draw.rect(screen,button.colors,(button.x,button.y,button.width,button.height))
-        button.draw_text(screen, text_font, button.textColor)
+    for button in Buttons: # Looper igennem alle knapper
+        if button.scene == Scene: # Checker hvis knappens navn er lig med scene variablen
+            pygame.draw.rect(screen,button.colors,(button.x,button.y,button.width,button.height)) # Tegner knappen
+            button.draw_text(screen, text_font, button.textColor) # Tegner teksten til skærmen
+
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                if button.is_clicked():
+                    Scene = button.is_clicked()
 
 
 
-    # RENDER YOUR GAME HERE
+
+
+
 
     # flip() the display to put your work on screen
     pygame.display.flip()
